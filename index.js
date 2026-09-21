de47977330d55ad1d3b227f36713a343bf3d4aa1
Wen_Xuan106 <yeewenxuan106@gmail.com>
2026-09-21 19:09:16 +0800

    Update baseHost URL for reverse proxy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Reverse proxy Worker: forwards all requests to baseHost.
  * Note: baseHost has NO trailing slash, because pathname already starts with "/".
  */
-const baseHost = 'https://www.ytdwlr.lol'
+const baseHost = 'https://ytdwlr.lol/'
 
 async function handleRequest (request) {
   const requestUrl = new URL(request.url)
