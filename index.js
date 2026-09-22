39280680f6a71ac1ebddb9aa24a3252b7cb5b100
Wen_Xuan106 <yeewenxuan106@gmail.com>
2026-09-22 08:20:42 +0800

    Fix baseHost URL in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Reverse proxy Worker: forwards all requests to baseHost.
  * Note: baseHost has NO trailing slash, because pathname already starts with "/".
  */
-const baseHost = 'https://cobalt.tool/'
+const baseHost = 'https://cobalt.tools/'
 
 async function handleRequest (request) {
   const requestUrl = new URL(request.url)
