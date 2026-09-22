3e7c1d7e920cfa07d327fc8cf5a03ca42f9d932b
Wen_Xuan106 <yeewenxuan106@gmail.com>
2026-09-22 08:23:23 +0800

    Change baseHost URL to meowing.de

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Reverse proxy Worker: forwards all requests to baseHost.
  * Note: baseHost has NO trailing slash, because pathname already starts with "/".
  */
-const baseHost = 'https://cobalt.tools/'
+const baseHost = 'https://meowing.de/'
 
 async function handleRequest (request) {
   const requestUrl = new URL(request.url)
